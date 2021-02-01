c3796e5626507b3eb15687b4cfadfa09ce42fa44
RYoT-a <ryota.web.bboy@gmail.com>
2021-02-01 13:56:01 +0900

    btn1

---
+++ menu/menu.js
@@ -0,0 +1,10 @@
+document.getElementById('#js_btn').onclick = function() {
+  let js_btn = document.getElementById('#js_btn');
+
+  if(js_btn.getAttribute('aria-expanded') == "false") {
+    js_btn.setAttribute('aria-expanded', 'true');
+
+  } else if(js_btn.getAttribute('aria-expanded') == "true") {
+    js_btn.setAttribute('aria-expanded', 'false');
+  }
+};
