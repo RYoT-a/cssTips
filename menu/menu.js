cf40190d5d5d336b70c73f5d1c078727cba294f7
RYoT-a <ryota.web.bboy@gmail.com>
2021-02-02 13:36:02 +0900

    ハンバーガーメニューにいろいろ追加

--- menu/menu.js
+++ menu/menu.js
@@ -1,3 +1,4 @@
+// icon1
 document.getElementById('js_btn').onclick = function() {
   let js_btn = document.getElementById('js_btn');
 
@@ -6,5 +7,41 @@ document.getElementById('js_btn').onclick = function() {
 
   } else if(js_btn.getAttribute('aria-expanded') == "true") {
     js_btn.setAttribute('aria-expanded', 'false');
+  }
+};
+
+// icon2
+document.getElementById('js_btn2').onclick = function() {
+  let js_btn2 = document.getElementById('js_btn2');
+
+  if(js_btn2.getAttribute('aria-expanded') == "false") {
+    js_btn2.setAttribute('aria-expanded', 'true');
+
+  } else if(js_btn2.getAttribute('aria-expanded') == "true") {
+    js_btn2.setAttribute('aria-expanded', 'false');
+  }
+};
+
+// icon3
+document.getElementById('js_btn3').onclick = function() {
+  let js_btn3 = document.getElementById('js_btn3');
+
+  if(js_btn3.getAttribute('aria-expanded') == "false") {
+    js_btn3.setAttribute('aria-expanded', 'true');
+
+  } else if(js_btn3.getAttribute('aria-expanded') == "true") {
+    js_btn3.setAttribute('aria-expanded', 'false');
+  }
+};
+
+// icon4
+document.getElementById('js_btn4').onclick = function() {
+  let js_btn4 = document.getElementById('js_btn4');
+
+  if(js_btn4.getAttribute('aria-expanded') == "false") {
+    js_btn4.setAttribute('aria-expanded', 'true');
+
+  } else if(js_btn4.getAttribute('aria-expanded') == "true") {
+    js_btn4.setAttribute('aria-expanded', 'false');
   }
 };
